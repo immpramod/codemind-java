55df101c5135179317cd70d6083f2361a071ba37
pramod kumar <95994718+immpramod@users.noreply.github.com>
2023-08-18 15:26:40 +0530

    import java.util.*; public class Pattren4 {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 if(i==j||(i+j==n+1))                 {                     System.out.print("x");                 }                 else                 {                     System.out.print("0");                 }             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Pattren4
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==j||(i+j==n+1))
+                {
+                    System.out.print("x");
+                }
+                else
+                {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
